14ab1c2c9efdfa7c7cf184fb558c1c45ff3bb5a6
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-09-28 22:12:12 +0700

    update: animaiton form

--- src/features/ContactPage/FormPage.tsx
+++ src/features/ContactPage/FormPage.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 
@@ -19,6 +19,7 @@ import Modal from "@/components/Modal/Modal";
 
 import { CgSpinner } from "react-icons/cg";
 import { BiCheckCircle } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 const FormPage = ({
   handleSubmit,
@@ -30,25 +31,47 @@ const FormPage = ({
   isShowOtherChannel,
   onSubmit,
   showModal,
-  setShowModal
+  setShowModal,
 }: WithFormPageProps) => {
- 
+  const titleAnimation = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delay: 1,
+        staggerChildren: 0.08,
+      },
+    },
+  };
+
+  const itemAnimation = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
 
   return (
     <>
-      <div className="relative bg-neutral-1000 rounded-xl w-full max-w-[1140px] mx-auto py-[30px] md:py-[50px] lg:px-[50px]  sm:my-0">
-   
-        <h1 className="text-center mb-20 text-white">
-          <div className="text-2xl font-light mr-20">Wanna talk with us?</div>
-          <div className="font-normal inline bg-line text-3xl md:text-5xl ml-20">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={titleAnimation}
+        className="relative bg-neutral-1000 rounded-xl w-full max-w-[1140px] mx-auto py-[30px] md:py-[50px] lg:px-[50px]  sm:my-0"
+      >
+        <motion.h1 className="text-center mb-20 text-white">
+          <motion.div variants={itemAnimation} className="text-2xl font-light">
+            Wanna talk with us?
+          </motion.div>
+          <motion.div
+            variants={itemAnimation}
+            className="font-normal inline bg-line text-3xl md:text-5xl text-primary-100"
+          >
             Contact us
-          </div>
-        </h1>
-        <form
-          className=" max-w-[1440px] px-5 md:px-5"
-          encType="multipart/form-data"
-          onSubmit={handleSubmit(onSubmit)}
-        >
+          </motion.div>
+        </motion.h1>
+        <motion.form     variants={itemAnimation}  onSubmit={handleSubmit(onSubmit)}>
           {/* row 1 */}
           <div className="text-white">
             <TextAreaField
@@ -251,7 +274,11 @@ const FormPage = ({
           </p>
 
           <div className=" flex justify-end">
-            <ExternalPrimaryButton id="form-submit-ContactForm" pathName="#" disabled={!isValid}>
+            <ExternalPrimaryButton
+              id="form-submit-ContactForm"
+              pathName="#"
+              disabled={!isValid}
+            >
               <div className="flex justify-center items-center gap-x-1">
                 {state.submitting && (
                   <span className=" animate-spin">
@@ -262,7 +289,7 @@ const FormPage = ({
               </div>
             </ExternalPrimaryButton>
           </div>
-        </form>
+        </motion.form>
 
         <StatusMessage errors={state.errors} />
         <SocialsSection />
@@ -278,17 +305,16 @@ const FormPage = ({
                 <BiCheckCircle />
               </div>
               <h3 className="text-xl sm:text-xl font-medium text-slate-900 mb-5">
-              Thank you for sharing your idea with us!
+                Thank you for sharing your idea with us!
               </h3>
               <div className="text-sm font-light mt-0 pt-0 text-slate-700 mb-5">
                 We will get back to you as soon as possible. Let the journey
                 with CodeWork
               </div>
-             
             </div>
           </div>
         </Modal>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/features/Homepage/OurPartnerSection/OurPartner.tsx
+++ src/features/Homepage/OurPartnerSection/OurPartner.tsx
@@ -9,28 +9,27 @@ export const OurPartner = () => {
   const [ref, inView] = useInView();
 
   const headingAnimation = {
-    hidden: { opacity: 0  }, // Fade in from left
+    hidden: { opacity: 0 }, // Fade in from left
     visible: { opacity: 1 },
   };
-
 
   return (
     <section className="relative py-12  md:py-[100px] mx-auto px-5 lg:px-[50px] sm:pt-0  bg-neutral-1000 ">
       <motion.div
         ref={ref}
         className={`
-        } flex flex-col-reverse lg:flex-row justify-center gap-10 items-center m-auto`}
+        } flex flex-col-reverse  items-center justify-center gap-10 items-center m-auto`}
       >
         {/* logo */}
-        <div className="grid grid-cols-3 sm:grid-cols-4 gap-5 ">
+        <div className="flex flex-wrap justify-center gap-5 max-w-[1000px]">
           {ourPartnerSectionText.imagesList.map((item: any, index: number) => (
             <React.Fragment key={`our-partner-${index}`}>
               <motion.a
-                      initial="hidden"
-                      animate={inView ? "visible" : "hidden"}
-                      variants={headingAnimation}
-                      transition={{ duration: 0.8 }}
-                className="w-[80px] h-[80px] sm:w-[110px] sm:h-[110px]  bg-white rounded-full border-2 border-primary-700 flex justify-center items-center transition-all ease-out  hover:shadow-lg"
+                initial="hidden"
+                animate={inView ? "visible" : "hidden"}
+                variants={headingAnimation}
+                transition={{ duration: 0.8 }}
+                className="w-[150px] h-[150px] sm:w-[200px] sm:h-[200px]  bg-white rounded-full border-2 border-primary-700 flex justify-center items-center transition-all  hover:shadow-lg"
                 href={item.urlName}
                 target="_blank"
                 rel="noopener nofollow"
@@ -53,23 +52,17 @@ export const OurPartner = () => {
           animate={inView ? "visible" : "hidden"}
           variants={headingAnimation}
           transition={{ duration: 0.8 }}
-          className="flex flex-col items-center lg:items-start transition-all ease-out  "
+          className="flex flex-col items-center transition-all "
         >
           <h2 className="text-xl md:text-4xl font-semibold flex mb-7">
             <span className="text-primary-100 border-b-2 border-primary-100 ">
-            
               PARTNERS
             </span>
           </h2>
           <Paragraph
-            className={`lg:max-w-[550px] xl:max-w-[670px] text-base sm:text-md font-light text-white text-center lg:text-left`}
+            className={`lg:max-w-[1000px] text-base sm:text-md font-light text-white text-center`}
           >
-            {`At CodeWork, we believe in the power of collaboration. That's why
-            we've teamed up with industry-leading partners who share our vision
-            for innovation and excellence. Together, we bring you cutting-edge
-            solutions that propel your business into the future. Explore our
-            network of trusted partners who help us deliver the technology
-            solutions you can rely on.`}
+            {`At CodeWork, we believe in the power of collaboration. That's why we've teamed up with industry-leading partners who share our vision for innovation and excellence. Together, we bring you cutting-edge solutions that propel your business into the future. Explore our network of trusted partners who help us deliver the technology solutions you can rely on.`}
           </Paragraph>
         </motion.div>
       </motion.div>
