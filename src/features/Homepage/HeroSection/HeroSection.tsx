94d3464634f3e216052de59751b9f19e995f5d39
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-09-28 10:39:26 +0700

    update: hero section

--- src/features/Homepage/HeroSection/HeroSection.tsx
+++ src/features/Homepage/HeroSection/HeroSection.tsx
@@ -1,36 +1,29 @@
 import React from "react";
-import { Gloria_Hallelujah } from "@next/font/google";
 import withHeroSection from "./withHeroSection";
-
-import Image from "next/image";
-
 import { HeroSectionProps } from "./interface";
 import Paragraph from "@/components/typography/paragraph/Paragraph";
 import { PrimaryButton } from "@/components/buttons/PrimaryButton";
-
-const fontGloriaHallelujah = Gloria_Hallelujah({
-  weight: ["400"],
-  subsets: ["latin"],
-  variable: "--font-gloria-hallelujah",
-});
 
 const HeroSection = (props: HeroSectionProps) => {
   const { heroSectionText } = props;
   return (
     <section
-      className={`relative  md:py-[50px] mx-auto px-5 lg:px-[50px] bg-primary-1000`}
+      className={`relative  md:py-[50px] mx-auto px-5 lg:px-[50px]  custom-bg `}
     >
-      <div className="relative flex w-full max-w-[1440px]">
+      <div className="relative flex w-full max-w-[1440px] m-auto ">
         <div className="z-0 w-[100%] lg:w-[100%]  min-h-[600px] relative flex flex-col justify-center sm:p-0">
           <h1 className="mb-5 md:mb-5 relative font-semibold">
-            <div className="text-lg md:text-6xl  mb-3 text-white">We can make</div>
-            <div className="text-lg md:text-6xl mb-3  text-white" >
-              your <span className="text-primary-100">idea</span> come <span className="text-primary-100">true</span>
+            <div className="text-3xl md:text-6xl mb-3 text-white">
+              We can make
+            </div>
+            <div className="text-3xl md:text-6xl mb-3  text-white">
+              your <span className="text-primary-100">idea</span> come 
+              <span className="text-primary-100"> true</span>
             </div>
           </h1>
 
           <Paragraph
-            className={`mb-8 lg:max-w-[550px]  xl:max-w-[670px] text-lg font-light text-white`}
+            className={`mb-8 lg:max-w-[550px]  xl:max-w-[670px] text-base sm:text-md font-light text-white`}
           >
             Turning your brilliant ideas into real-world solutions is our
             passion! Whether you're an entrepreneur, a startup, or a
@@ -43,15 +36,8 @@ const HeroSection = (props: HeroSectionProps) => {
               Contact us
             </PrimaryButton>
           </div>
-
-         
         </div>
-
-        {/* right box  */}
-        {/* <div className="hidden lg:block lg:w-[20%]"></div> */}
-
       </div>
-     
     </section>
   );
 };
