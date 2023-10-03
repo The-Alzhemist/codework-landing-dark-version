c1bc726edfa94b16b1e3e9b961940d5fccbfe70a
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-10-03 17:13:44 +0700

    add: meta tag

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,5 @@
 import FormPage from "@/features/ContactPage/FormPage";
+import { Metadata } from "next";
 import { Poppins } from 'next/font/google'
 
 
@@ -6,6 +7,32 @@ const poppinsFont = Poppins({
   weight: ['100', '300',  '500', '700'],
   subsets: ['latin'],
 })
+
+export const metadata: Metadata = {
+  title: "CodeWork Tech | Contact Us",
+  description: "Get in touch with us and make your dream come true. Let’s create something extraordinary together!",
+  openGraph: {
+    title: "CodeWork Tech | Contact Us",
+  description: "Get in touch with us and make your dream come true. Let’s create something extraordinary together!",
+    url: 'https://codework-tech.com/contact-us',
+    siteName: 'https://codework-tech.com/contact-us',
+    images: [
+      {
+        url: '/logo/meta/metatag-contact-us.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/logo/meta/metatag-contact-us.png',
+        width: 1800,
+        height: 1600,
+        alt: 'codework-tech-contact-us-logo-white-transpatent',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
 
 export default function ContactUsPage() {
   
