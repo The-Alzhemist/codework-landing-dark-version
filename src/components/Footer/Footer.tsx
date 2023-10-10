bf1e9c688d49d94306bf1ddbca689e16fedc2c6d
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-10-10 11:23:39 +0700

    update: footer animation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import withFooter from "./withFooter";
 import { FooterMenuItem, FooterProps } from "./interface";
 import Link from "next/link";
-import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
 // เปิดใช้งาน ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
@@ -14,25 +14,36 @@ const Footer = (props: FooterProps) => {
 
   const footerLeft = useRef<HTMLDivElement | null>(null);
   const footerRight = useRef<HTMLDivElement | null>(null);
-  const pref = useRef<HTMLDivElement | null>(null);
-  const divBtnRef = useRef<HTMLDivElement | null>(null);
   const root = useRef<any>(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     let ctx = gsap.context(() => {
-      gsap.from(footerLeft.current, {opacity:0, delay: 0, y:-50, duration:3, ease: "power4", scrollTrigger: {
-        trigger: footerLeft.current,
-        toggleActions: "restart none none none",
-        
-      } });
-      gsap.from(footerRight.current, {opacity:0, delay: 0, y:50, duration:3, ease: "power4", scrollTrigger: {
-        trigger: footerRight.current,
-        toggleActions: "restart none none none",
-      } });
+      gsap.from(footerLeft.current, {
+        x: 50,
+        opacity: 0,
+        duration: 2,
+        ease: "Power4.easeInOut",
+        scrollTrigger: {
+          trigger: footerLeft.current,
+          markers: true,
+          toggleActions: "restart none none none"
+        },
+      })
+      gsap.from(footerRight.current, {
+        x: -50,
+        opacity: 0,
+        duration: 2,
+        ease: "Power4.easeInOut",
+        scrollTrigger: {
+          trigger: footerRight.current,
+          markers: true,
+          toggleActions: "restart none none none"
+        },
+      });
     }, root);
 
     return () => ctx.revert();
-  },[]);
+  }, []);
 
   return (
     <footer className="bg-neutral-1000 text-white">
@@ -42,46 +53,70 @@ const Footer = (props: FooterProps) => {
         {/* left */}
         <div className="w-full lg:w-1/2" ref={footerLeft}>
           <div className="font-light">{footerMenu.footerLeft.companyName}</div>
-          <div className="font-light">{footerMenu.footerLeft.companyAddress}</div>
-          <div className="font-light">Email: {footerMenu.footerLeft.companyEmail}</div>
-          <div className="mb-5 font-light"> Tel: {footerMenu.footerLeft.companyTel}</div>
-          <div className="text-xs sm:text-sm font-light">
+          <div className="font-light">
+            {footerMenu.footerLeft.companyAddress}
+          </div>
+          <div className="font-light">
+            Email: {footerMenu.footerLeft.companyEmail}
+          </div>
+          <div className="mb-5 font-light">
+            {" "}
+            Tel: {footerMenu.footerLeft.companyTel}
+          </div>
+          <div className="text-xs  font-light">
             {footerMenu.footerLeft.companyCopyright}
           </div>
         </div>
 
         {/* right */}
-      
-        <div className="w-full lg:w-1/2 flex flex-col gap-y-5" ref={footerRight}>
-            {/* Row:1 */}
+
+        <div
+          className="w-full lg:w-1/2 flex flex-col gap-y-5"
+          ref={footerRight}
+        >
+          {/* Row:1 */}
           <div className="w-full flex">
             {footerMenu.footerRight
               .slice(0, 3)
               .map((menu: FooterMenuItem, index: number) => (
                 <React.Fragment key={`footer-menu-${index}`}>
                   <div className="w-1/3">
-                  <Link href={menu.path}>{menu.pageMenu}</Link>
-                      {menu.hasSubMenu &&
-                        menu.subMenu?.map((submenu: { name: string }, index: number) => (
-                          <div className="text-xs sm:text-sm font-light" key={`submenu-top-${index}`}>{submenu.name}</div>
-                        ))}
-                    </div>
+                    <Link href={menu.path}>{menu.pageMenu}</Link>
+                    {menu.hasSubMenu &&
+                      menu.subMenu?.map(
+                        (submenu: { name: string }, index: number) => (
+                          <div
+                            className="text-xs sm:text-sm font-light"
+                            key={`submenu-top-${index}`}
+                          >
+                            {submenu.name}
+                          </div>
+                        )
+                      )}
+                  </div>
                 </React.Fragment>
               ))}
           </div>
-            {/* Row:2 */}
+          {/* Row:2 */}
           <div className="w-full flex">
-          {footerMenu.footerRight
+            {footerMenu.footerRight
               .slice(3, 6)
               .map((menu: FooterMenuItem, index: number) => (
                 <React.Fragment key={`footer-menu-${index}`}>
                   <div className="w-1/3">
                     <Link href={menu.path}>{menu.pageMenu}</Link>
-                      {menu.hasSubMenu &&
-                        menu.subMenu?.map((submenu: { name: string }, index: number) => (
-                          <div className="text-xs sm:text-sm" key={`submenu-top-${index}`}>{submenu.name}</div>
-                        ))}
-                    </div>
+                    {menu.hasSubMenu &&
+                      menu.subMenu?.map(
+                        (submenu: { name: string }, index: number) => (
+                          <div
+                            className="text-xs sm:text-sm"
+                            key={`submenu-top-${index}`}
+                          >
+                            {submenu.name}
+                          </div>
+                        )
+                      )}
+                  </div>
                 </React.Fragment>
               ))}
           </div>
