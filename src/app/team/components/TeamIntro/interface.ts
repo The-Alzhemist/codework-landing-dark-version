9d7ef3f7da162487fe2db6225b45ad0b680723f3
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-10-25 09:53:08 +0700

    add: team page.tsx

---
+++ src/app/team/components/TeamIntro/interface.ts
@@ -0,0 +1,12 @@
+
+
+export interface TeamIntroProps {
+    root: React.RefObject<HTMLElement>;
+    h1Ref: React.RefObject<HTMLDivElement>;
+    pRef: React.RefObject<HTMLDivElement>;
+    btnRef: React.RefObject<HTMLDivElement>;
+    circleLeftRef: React.RefObject<HTMLDivElement>;
+    circleRightRef: React.RefObject<HTMLDivElement>;
+  }
+  
+  
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,6 +2,7 @@ import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 import FormPage from "@/features/ContactPage/FormPage";
 import { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import TeamIntro from "./components/TeamIntro/TeamIntro";
 
 const poppinsFont = Poppins({
   weight: ["100", "300", "500", "700"],
@@ -39,19 +40,23 @@ export const metadata: Metadata = {
 export default function TeamPage() {
   return (
     <main className={` bg-neutral-1000 ${poppinsFont.className}`}>
-      <div className="relative bg-neutral-1000 rounded-xl w-full max-w-[1140px] mx-auto py-[30px] md:py-[50px] lg:px-[50px] sm:my-0 px-5">
-        <div className="text-center h-screen flex justify-center flex-col items-center text-4xl text-white">
-      
-          <div className="mb-10"> Service page is coming soon...</div>
-          <PrimaryButton
-          className=" py-3 text-base sm:text-lg hover:scale-90 mb-5 lg:mb-0"
-          pathName="/"
-        >
-          Back to home page
-        </PrimaryButton>
-        </div>
-       
+    {/* <div className="relative bg-neutral-1000 rounded-xl w-full max-w-[1140px] mx-auto py-[30px] md:py-[50px] lg:px-[50px] sm:my-0 px-5">
+      <div className="text-center h-screen flex justify-center flex-col items-center text-4xl text-white">
+    
+        <div className="mb-10"> Service page is coming soon...</div>
+        <PrimaryButton
+        className=" py-3 text-base sm:text-lg hover:scale-90 mb-5 lg:mb-0"
+        pathName="/"
+      >
+        Back to home page
+      </PrimaryButton>
       </div>
-    </main>
+     
+    </div> */}
+    
+    <TeamIntro/>
+
+    {/* <ContactHomeSection/> */}
+  </main>
   );
 }
