411a7411bd5acdb58b36f887f5c8ed879b4cf74d
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-11-23 21:50:24 +0700

    add(storyblok):setup

---
+++ src/features/BlogPage/BlogPage.tsx
@@ -0,0 +1,23 @@
+
+
+import { ISbStoriesParams, getStoryblokApi} from "@storyblok/react/rsc";
+import StoryblokStory from "@storyblok/react/story";
+import BlogIntro from "./components/BlogIntro/BlogIntro";
+ 
+export default async function BlogWrapper() {
+  const { data } = await fetchData();
+ 
+  return (
+    <div>
+      <BlogIntro/>
+      <StoryblokStory story={data.story} />
+    </div>
+  );
+}
+ 
+ async function fetchData() {
+  let sbParams:ISbStoriesParams = { version: "draft" };
+ 
+  const storyblokApi = getStoryblokApi();
+  return storyblokApi.get(`cdn/stories/blog`, sbParams);
+}
