4cd7b5324a7e40c46134f87853d961aad361e397
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-11-24 08:24:37 +0700

    update(storyblok): blogCard

--- src/app/blog/components/Feature.js
+++ src/app/blog/components/Feature.js
@@ -1,9 +1,9 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
- 
+
 const Feature = ({ blok }) => (
-  <div  {...storyblokEditable(blok)}>
+  <div {...storyblokEditable(blok)}>
     {blok.name}
   </div>
 );
- 
+
 export default Feature;
