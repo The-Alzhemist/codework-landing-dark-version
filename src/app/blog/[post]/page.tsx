113810a4e96022a42bc6a0264d1680514ee07794
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-11-27 16:19:41 +0700

    update(blofDetail): show detail

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -4,6 +4,7 @@
 import React from 'react'
 import BlogPostWrapper from '@/features/BlogPage/components/BlogPost/BlogPostWrapper';
 import { Poppins } from 'next/font/google';
+import { ISbStoriesParams, StoryblokStory, getStoryblokApi } from '@storyblok/react/rsc';
 
 const poppinsFont = Poppins({
   weight: ["100", "300", "500", "700"],
@@ -13,13 +14,24 @@ const poppinsFont = Poppins({
 });
 
 
-export default async function PostPage() {
+export default async function PostPage({params}: any) {
+  console.log("params >>", params.post)
+  const { data } = await fetchData(params.post);
 
   return (
     <main className={` ${poppinsFont.className} relative md:py-[50px] mx-auto px-5 lg:px-[200px] bg-neutral-1000`}>
-      <BlogPostWrapper/>
+      <StoryblokStory story={data.story}/>
     </main>
   );
 }
  
+
+async function fetchData(post:any) {
+  let sbParams: ISbStoriesParams = { version: "draft" };
+  
+  const storyblokApi = getStoryblokApi();
+  return storyblokApi.get(`cdn/stories/blog/${post}`, sbParams);
+}
+
+
 
