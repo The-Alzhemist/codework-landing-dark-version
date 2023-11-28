acd012a6da7237fae3480594c37ef5444345e771
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-11-28 14:47:54 +0700

    update: blog loop

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,10 +1,11 @@
-
-
-
-import React from 'react'
-import BlogPostWrapper from '@/features/BlogPage/components/BlogPost/BlogPostWrapper';
-import { Poppins } from 'next/font/google';
-import { ISbStoriesParams, StoryblokStory, getStoryblokApi } from '@storyblok/react/rsc';
+import React from "react";
+import BlogPostWrapper from "@/features/BlogPage/components/BlogPost/BlogPostWrapper";
+import { Poppins } from "next/font/google";
+import {
+  ISbStoriesParams,
+  StoryblokStory,
+  getStoryblokApi,
+} from "@storyblok/react/rsc";
 
 const poppinsFont = Poppins({
   weight: ["100", "300", "500", "700"],
@@ -13,25 +14,50 @@ const poppinsFont = Poppins({
   adjustFontFallback: false,
 });
 
-
-export default async function PostPage({params}: any) {
-  console.log("params >>", params.post)
+export default async function PostPage({ params }: any) {
   const { data } = await fetchData(params.post);
 
   return (
-    <main className={` ${poppinsFont.className} relative md:py-[50px] mx-auto px-5 lg:px-[200px] bg-neutral-1000`}>
-      <StoryblokStory story={data.story}/>
+    <main
+      className={` ${poppinsFont.className} relative md:py-[50px] mx-auto px-5 lg:px-[200px] bg-neutral-1000`}
+    >
+      <StoryblokStory story={data.story} />
     </main>
   );
 }
- 
 
-async function fetchData(post:any) {
+async function fetchData(post: any) {
   let sbParams: ISbStoriesParams = { version: "draft" };
-  
+
   const storyblokApi = getStoryblokApi();
   return storyblokApi.get(`cdn/stories/blog/${post}`, sbParams);
 }
 
+export async function generateStaticParams() {
+  const resultArray: any = [];
+  const storyblokApi = getStoryblokApi();
+  const links = storyblokApi
+    .get("cdn/links/", {
+      starts_with: "blog/",
+    })
+    .then((response) => {
 
+      const linkObj = [response.data.links];
 
+      for (const item of linkObj) {
+        // Loop through the inner objects
+        for (const key in item) {
+          if (item.hasOwnProperty(key)) {
+            const realPath = item[key].real_path;
+            resultArray.push({ path: realPath });
+          }
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  // console.log('links #1 >>>>> ', links)
+  console.log('resultArray >>>>>>',resultArray )
+  return [{ post: "1" }, { post: "2" }, { post: "3" }];
+}
--- src/app/blog/components/PostDetail/Image.js
+++ src/app/blog/components/PostDetail/Image.js
@@ -2,7 +2,11 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 
 const Image = ({ blok }) => (
   <div {...storyblokEditable(blok)} className="relative">
-   <img  className="w-full" src="https://fakeimg.pl/400x100" />
+   <img  className="w-full" src={
+              blok.Image.filename
+                ? blok.Image.filename
+                : "https://fakeimg.pl/30x30/282828/eae0d0/?retina=1"
+            } />
   </div>
 );
 
