61d0865b2dfd1dc6649e31d14ea025b370807965
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-12-26 14:36:28 +0700

    update(social list): add

--- src/components/SocialContactFloating/SocialContactFloating.tsx
+++ src/components/SocialContactFloating/SocialContactFloating.tsx
@@ -1,43 +1,48 @@
-import React from "react";
+// SocialContactFloating.js
+import React, { useState } from "react";
 
-export default function SocialContactFloating() {
+const SocialContactFloating = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleListClick = () => {
+    // Do something when a social item is clicked
+  };
+
   return (
-    <div className="flex flex-col gap-y-2 fixed bottom-4 right-4 z-40">
-      <a
-        href="https://page.line.me/448yyxgh"
-        rel="nofollow"
-        aria-label="Contact us with line"
-        target="_blank"
+    <div
+      className="relative inline-block"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none"
+        onClick={handleListClick}
       >
-        <picture>
-          <source srcSet="/logo/codework-line-logo-v1.webp" type="image/webp" />
-          <img
-            src="/logo/codework-line-logo-v1.png"
-            alt="contact line"
-            title="contact line"
-            loading="lazy"
-            className="h-[50px] w-[50px] sm:h-[70px] sm:w-[70px] z-50"
-          />
-        </picture>
-      </a>
+        Social #1
+      </button>
 
-      <a
-        href="https://www.facebook.com/profile.php?id=61551048177724"
-        rel="nofollow"
-        aria-label="Contact us with facebook page"
-        target="_blank"
-      >
-        <picture>
-          <source srcSet="/logo/codework-fb-logo-v1.webp" type="image/webp" />
-          <img
-            src="/logo/codework-fb-logo-v1.png"
-            alt="contact facebook"
-            title="contact facebook"
-            loading="lazy"
-            className="h-[50px] w-[50px] sm:h-[70px] sm:w-[70px]"
-          />
-        </picture>
-      </a>
+      {isHovered && (
+        <div
+          className="absolute top-10 left-0 p-4 bg-white shadow-md rounded-md z-10 cursor-pointer"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <p className="hover-item hover:text-yellow-200">Facebook</p>
+          <p className="hover-item hover:text-yellow-200">Line</p>
+          <p className="hover-item hover:text-yellow-200">Twitter</p>
+          <p className="hover-item hover:text-yellow-200">GitHub</p>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default SocialContactFloating;
