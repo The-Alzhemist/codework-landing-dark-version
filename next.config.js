77222a6ec4ea5a47532d0bdc951750fc34ac14da
Aphiwit1 <Aphiwit.boon@gmail.com>
2024-01-08 17:43:48 +0700

    update(nextconfig): add export

--- next.config.js
+++ next.config.js
@@ -2,20 +2,9 @@
 const withNextIntl = require('next-intl/plugin')();
 
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/old-blog/:slug',
-            destination: '/news/:slug', // Matched parameters can be used in the destination
-            permanent: true,
-          },
-        ]
-      },
-    output: 'standalone',
+    output: 'export',
     images: { unoptimized: false },
 }
-
-
 
 
 module.exports = withNextIntl(nextConfig);
